53a83d3ed91867943d0882b67e1af6a52c5d9677
Ted Moskovitz <thmoskovitz@gmail.com>
2024-02-11 20:41:43 +0000

    adding vector addition

--- Chapter2_Heterogeneous-Data-Parallel-Computing/vector_addition.cu
+++ Chapter2_Heterogeneous-Data-Parallel-Computing/vector_addition.cu
@@ -1,12 +1,37 @@
 // compilation: nvcc vector_addition.cu -o vector_addition
 // usage: ./vector_addition
+#include <stdio.h>
+#include <time.h>
 
-#define N 1000
+#define N 100000000
 
 void vecAddRegular(float* A_h, float* B_h, float* C_h, int n) {
     for (int i = 0; i < n; i++) {
         C_h[i] = A_h[i] + B_h[i];
     }
+}
+
+void vecAdd(float* A_h, float* B_h, float* C_h, int n) {
+    int size = sizeof(float) * n; 
+    // on-device versions
+    float *A_d, *B_d, *C_d; 
+    // allocate memory on device
+    cudaMalloc((void**)&A_d, size); 
+    cudaMalloc((void**)&B_d, size);
+    cudaMalloc((void**)&C_d, size);
+    // copy data from host to device
+    cudaMemcpy(A_d, A_h, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(B_d, B_h, size, cudaMemcpyHostToDevice);
+
+
+
+    // copy result back to host
+    cudaMemcpy(C_h, C_d, size, cudaMemcpyDeviceToHost);
+
+    // free on-device memory
+    cudaFree(A_d);
+    cudaFree(B_d);
+    cudaFree(C_d);
 }
 
 int main() {
@@ -16,9 +41,10 @@ int main() {
 
     // allocate memory -- note regular C
     // malloc only takes size as input
-    A = (float*)malloc(sizeof(float) * N); 
-    B = (float*)malloc(sizeof(float) * N);
-    C = (float*)malloc(sizeof(float) * N);
+    int size = sizeof(float) * N;
+    A = (float*)malloc(size); 
+    B = (float*)malloc(size);
+    C = (float*)malloc(size);
 
     // initialize
     for (int i = 0; i < N; i++) {
@@ -26,8 +52,18 @@ int main() {
         B[i] = 2.0f;
     }
 
-    // run addition
+    // run regular addition
+    clock_t start = clock();
     vecAddRegular(A, B, C, N); 
+    // Record the end time
+    clock_t end = clock();
+    // Calculate the elapsed time in seconds
+    double elapsed_time = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("Regular vector addition: %.3f seconds\n", elapsed_time);
+
+
+    // now do the parallel / on-device version
+
 
 }
 
